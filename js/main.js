4402cb3713845302904fdae1a4d732b0d78249b1
satayam gautam <gautamsatyam591@gmail.com>
2025-12-17 15:52:27 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Lightbox
+function openLightbox(src) {
+  document.getElementById("lightbox-img").src = src;
+  document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
